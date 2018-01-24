d83ebfa67f615ea40bde230d5b58fda95d69d328
Wei Yang <weiyangmiami@gmail.com>
2018-01-24 07:24:12 -0600

    Initial Commit

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CONFIG ={
+    apiKey: "AIzaSyCMNvnZh2Gp08prpSPvtFFFvyZePK0LBmc",
+    authDomain: "crud-66fec.firebaseapp.com",
+    databaseURL: "https://crud-66fec.firebaseio.com",
+    projectId: "crud-66fec",
+    storageBucket: "crud-66fec.appspot.com",
+    messagingSenderId: "967452973456"
+}
